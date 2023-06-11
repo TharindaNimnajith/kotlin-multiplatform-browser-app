52a326341353154ac2d534221eda27850f022866
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-06-11 15:50:00 +0530

    init

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,8 +1,8 @@
+import kotlinx.browser.document
+import kotlinx.browser.window
 import kotlinx.html.div
 import kotlinx.html.dom.append
 import org.w3c.dom.Node
-import kotlinx.browser.document
-import kotlinx.browser.window
 
 fun main() {
     window.onload = { document.body?.sayHello() }
